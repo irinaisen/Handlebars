e5854e5c8b55343ef9146a3b4fedbf07ad95d6b4
irinaisen <irina.paivoke@hotmail.com>
2024-04-13 20:37:00 +0300

    Otsikot

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ app.set('view engine','handlebars');
 
 app.get('/', (req,res) => {
  //   res.send("Testing");
- res.render('index');
+// res.render('index');
+    res.render('index',
+    {
+        title: 'Home',
+        companyName: 'Business'
+    });
 });
+
+app.get('/contact', (req,res) => {
+    //   res.send("Testing");
+    res.render('contact');
+   });
+
+   //Folder for static files like css, jpg...
+   app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
